677a50729bcf0bb9d120dc817ac2ca1b13f3a324
Péter Kovács <peter.kovacs0519@gmail.com>
2019-02-16 13:41:32 +0100

    LFLT-340 Implement Hystrix wrapping over Bridge calls  - Updated RCP (Hystrix support ready)  - Aligned RequestContextAwareTaskDecorator to support Hystrix  - Fixed ArticleContentRequestAdapter (it was trying to combine response with null entry)  - Fixed CommonPageDataInterceptor (intercept request dispatchers only)  - Added default Hystrix configuration

--- web/src/main/java/hu/psprog/leaflet/lcfa/web/config/RequestContextAwareTaskDecorator.java
+++ web/src/main/java/hu/psprog/leaflet/lcfa/web/config/RequestContextAwareTaskDecorator.java
@@ -1,5 +1,6 @@
 package hu.psprog.leaflet.lcfa.web.config;
 
+import com.netflix.hystrix.strategy.concurrency.HystrixRequestContext;
 import org.springframework.core.task.TaskDecorator;
 import org.springframework.web.context.request.RequestAttributes;
 import org.springframework.web.context.request.RequestContextHolder;
@@ -14,12 +15,15 @@ public class RequestContextAwareTaskDecorator implements TaskDecorator {
     @Override
     public Runnable decorate(Runnable runnable) {
         RequestAttributes context = RequestContextHolder.getRequestAttributes();
+        HystrixRequestContext hystrixRequestContext = HystrixRequestContext.getContextForCurrentThread();
         return () -> {
             try {
                 RequestContextHolder.setRequestAttributes(context);
+                HystrixRequestContext.setContextOnCurrentThread(hystrixRequestContext);
                 runnable.run();
             } finally {
                 RequestContextHolder.resetRequestAttributes();
+                HystrixRequestContext.setContextOnCurrentThread(null);
             }
         };
     }
